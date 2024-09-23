bf969237baab9e88301e9e61755aa347d15b17a4
Developer@2446 <balu2446madhu@gmail.com>
2024-09-23 23:34:49 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,7 +1,11 @@
+import { config } from './src/config/config';
 import app from './src/app';
 
+
 const server=()=>{
-    const port=process.env.PORT ||3000;
+    console.log(config.port);
+    
+    const port=config.port ;
 
     app.listen(port,()=>{
         console.log(`server is listening on ${port}`);

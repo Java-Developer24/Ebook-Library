7a63127ff7bec6b4d27be1e136a1ce06dbe485ea
Developer@2446 <balu2446madhu@gmail.com>
2024-09-23 23:06:49 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import app from './src/app';
+
+const server=()=>{
+    const port=process.env.PORT ||3000;
+
+    app.listen(port,()=>{
+        console.log(`server is listening on ${port}`);
+        
+    })
+
+};
+
+server();

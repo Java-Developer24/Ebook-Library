6a1b8530687e051b50a2ea581b94b1eff28bcd3c
Developer@2446 <balu2446madhu@gmail.com>
2024-09-24 22:30:55 +0530

    add error handling

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,8 @@ conf();
 const _config={
 port:process.env.PORT,
 databaseURL:process.env.MONGO_CONNECTION_STRING,
-env : process.env.NODE_ENV
+env : process.env.NODE_ENV,
+jwtSecret:process.env.JwT_SECRET,
 };
 
 export const config=Object.freeze(_config);//freeze helps us to make the object immutable/unchangeable
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express"
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
+import { User } from "./userTypes";
 
 
 const createUser= async (
@@ -19,25 +22,49 @@ const createUser= async (
         
     }
     //database call
-    const user=await userModel.findOne({email:email})
+    try {
+        const user=await userModel.findOne({email:email})
     if (user) {
         const error=createHttpError(400,"User already exist with this email");
         return next(error);
     }
+    } catch (error) {
+        return next(createHttpError(500,`Error while getting user ${error}`))
+        
+    }
+    
     // password->hash
     const hashPassword= await bcrypt.hash(password,10);
+let newUser:User;
+    try {
+         newUser= await userModel.create({
+            name,
+            email,
+            password:hashPassword,
+        })
+        
+    } catch (error) {
+        return next(createHttpError(500,`error while creating user ${error}`))
+        
+    }
 
-    const newUser=userModel.create({
-        name,
-        email,
-        password:hashPassword,
-    })
+    
+    //token generation JWT
+    try {
+        const token=sign({sub:newUser._id},config.jwtSecret as string,{expiresIn:"7d"});
+
 
     //process
     //response
     res.json({
-        id:newUser._id
+        id:newUser._id,
+        accessToken:token,
     })
+    } catch (error) {
+        return next(createHttpError(500,`error while signing the jwt toeken ${error}`))
+        
+    }
+    
     next();
 
 }

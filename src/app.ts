b22490300afe72b4d988792a36964462e24702fb
Developer@2446 <balu2446madhu@gmail.com>
2024-09-24 23:20:30 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import globalErrorHandler from './middlewares/globalErrorHandler';
 
 import express  from "express"
 import userRouter from './user/userRouter';
+import bookRouter from './book/bookRouter';
 
 const app =express();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.get("/",(req,res)=>{
    res.json({message:"Welcome to Elib API's"});
 });
 app.use("/api/users",userRouter);
+app.use("/api/user/books",bookRouter)
 //global error handler
 app.use(globalErrorHandler);
     
